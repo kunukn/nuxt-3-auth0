65015b717e0228920cddffcb9c123cd16183f6d8
Kunuk Nykjær <kunukn@gmail.com>
2024-04-20 15:30:19 +0200

    files

---
+++ create-env-file.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+const filename = '.env'
+
+const content = `
+VITE_AUTH0_DOMAIN=<YOUR-DOMAIN>.auth0.com
+VITE_AUTH0_DOMAIN_URL=https://<YOUR-DOMAIN>.auth0.com
+VITE_AUTH0_CLIENT_ID=abcdefgh12345678
+VITE_AUTH0_SCOPE=openid profile email offline_access
+VITE_AUTH0_AUDIENCE=<AUDIENCE>
+VITE_AUTH0_CALLBACK_URL=/auth-callback
+VITE_BASE_API_URL=https://localhost:44394
+`
+
+fs.access(filename, fs.F_OK, (err) => {
+  if (err) {
+    fs.writeFile(filename, content, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('.env file was created', content)
+    })
+  }
+})
